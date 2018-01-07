ee001c7ebc34b80f7c9b9576ec71daa334f9ef00
max-sindi <sindm93@mail.ru>
2018-01-07 22:51:44 +0200

    top-section

---
+++ docs/js/bundle.js
@@ -0,0 +1,103 @@
+'use strict';
+
+$(document).ready(function () {
+  navTogglerCreate();
+
+  function navTogglerCreate() {
+    var toggler = $('#top-nav__toggler'),
+        toggleTarget = $('#top-nav'),
+        wind = $(window),
+        togglerIsVisible = void 0; // boolean in next steps
+
+
+    checkWidth();
+    wind.on('resize', checkWidth);
+
+    function checkWidth() {
+      var needShow = windowIsSmall();
+      // first time will not work because togglerIsVisible is void yet
+      // and will checked when window.resize
+      if (needShow === togglerIsVisible) {
+        return;
+      }
+
+      if (needShow) {
+        showToggler();
+      } else {
+        hideToggler();
+      }
+    }
+
+    function windowIsSmall() {
+      if (wind.width() <= 992) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function hideToggler() {
+      toggler.hide();
+      toggleTarget.show();
+      togglerIsVisible = false;
+    }
+
+    function showToggler() {
+      toggler.show();
+      toggleTarget.hide();
+      togglerIsVisible = true;
+    }
+  }
+
+  toggleNavVisibility();
+
+  function toggleNavVisibility() {
+    $('#js-nav-toggler').click(toggleVisible);
+
+    function toggleVisible() {
+      var targetToggle = $('#top-nav'),
+          isTargetHidden = $('#top-nav:hidden')[0],
+          targetBackSide = $('.top-section__main');
+
+      if (isTargetHidden) {
+        targetToggle.fadeIn('fast');
+        targetBackSide.addClass('small-visibility');
+      } else {
+        targetToggle.fadeOut('fast');
+        targetBackSide.removeClass('small-visibility');
+      }
+    }
+  }
+
+  topScrollDown();
+
+  function topScrollDown() {
+
+    $('#scroll-down-l').show();
+    $('#scroll-down-button').click(doScroll);
+
+    function doScroll() {
+      var page = $(window),
+          sectionHeight = $('#top-section').outerHeight(),
+          currentScroll = page.scrollTop(),
+          needScroll = sectionHeight - currentScroll,
+          i = 0,
+          imax = 100,
+          // fluency
+      interval = 5,
+          // frequency of scroll
+      part = needScroll / imax,
+          timer = null;
+
+      timer = setInterval(function () {
+        page.scrollTop(currentScroll + part * ++i);
+
+        if (i === imax) {
+          clearInterval(timer);
+        }
+      }, interval);
+    }
+  }
+
+  // $('.js-toggler').click();
+});
---
+++ gulpfile.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const
+		autoprefixer = require('autoprefixer'),
+		babel = require('gulp-babel'),
+		browserSync = require('browser-sync').create(),
+		cssnano = require('cssnano'),
+		debug = require('gulp-debug'),
+		gulp = require('gulp'),
+		less = require('gulp-sass'),
+		// nsg = require('node-sprite-generator'),
+		jsConcat = require('gulp-concat'),
+		imgMin = require('gulp-image'),
+		multipipe = require('multipipe'),
+		notify = require('gulp-notify'),
+		postcss = require('gulp-postcss'),
+		postUncss = require('postcss-uncss'),
+		pug = require('gulp-pug'),
+		rename = require('gulp-rename'),
+		sass = require('gulp-sass'),
+		sourcemaps = require('gulp-sourcemaps'),
+		tinify = require('tinify'),
+		uglify = require('gulp-uglify');
+
+let buildPath = './docs';
+
+/*
+	Development tasks
+*/
+gulp.task("pug:compile", function() {
+	return multipipe(
+					gulp.src('./source/pug/**/*.pug'),
+					pug({
+						debug: false,
+						compileDebug: true,
+						pretty: true
+					}),
+					gulp.dest(`${buildPath}/`)
+				).on('error', notify.onError());
+});
+
+
+gulp.task("less:compile", function() {
+
+		return gulp.src('./source/less/**/*.less')
+				.pipe( sourcemaps.init() )
+				.pipe( less() )
+				.pipe( sourcemaps.write('.') )
+				.pipe( gulp.dest(`${buildPath}/css`) );
+
+});
+
+
+gulp.task('sass:compile', function() {
+
+		return multipipe(
+				gulp.src('./source/sass/**/*.scss'),
+				sourcemaps.init(),
+				sass(),
+				sourcemaps.write('.'),
+				gulp.dest(`${buildPath}/css`)
+		).on( 'error', notify.onError() );
+
+});
+
+
+gulp.task('js', function() {
+	return gulp.src('./source/js/*.js')
+		.pipe(rename('bundle.js'))
+		.pipe(gulp.dest(`${buildPath}/js/`));
+})
+
+
+gulp.task('img', function() {
+	return gulp.src('./source/img/*.*')
+		.pipe(gulp.dest(`${buildPath}/img/`));
+})
+
+gulp.task('font', function() {
+	return gulp.src('./source/font/*.*')
+		.pipe(gulp.dest(`${buildPath}/font/`));
+})
+
+/*
+		DEFAULT
+*/
+gulp.task("default", ['sass:compile', 'pug:compile', 'js'],function() {
+
+		browserSync.init({
+				server: {
+						baseDir: `${buildPath}`
+				}
+		});
+
+		gulp.watch('source/sass/**/*.scss', ['sass:compile'])
+				.on('error', notify.onError());
+		gulp.watch('source/pug/**/*.pug',   ['pug:compile']);
+		gulp.watch('source/js/*.js',        ['js']);
+		gulp.watch('source/img/**/*.*',     ['img']);
+		gulp.watch('source/fonts/**/*.*',     ['font']);
+
+		gulp.watch(`${buildPath}/**/*.*`).on('change', browserSync.reload);
+});
+
+
+
+/*
+Build tasks
+*/
+
+gulp.task("img:min", function() {
+	gulp.src('./source/img/*.png')
+			.pipe( imgMin() )
+			.pipe(gulp.dest(`${buildPath}/img/`));
+});
+
+
+gulp.task('js:build', function() {
+
+  multipipe(
+		gulp.src([`./source/js/*.js`, `./source/js/modules/*.js`]),
+		jsConcat('bundle.js'),
+		babel(),
+		gulp.dest(`${buildPath}/js/`), // write not minified bundle.js
+		uglify(),
+		rename('bundle.min.js'),
+		gulp.dest(`${buildPath}/js/`)
+	)
+
+});
+
+
+// for build task
+gulp.task("pug:build", function() {
+
+	gulp.src('./source/pug/**/*.pug')
+			.pipe( pug() )
+			.pipe(gulp.dest(`${buildPath}/`));
+
+});
+
+gulp.task('css:build', ['sass:compile'], function() {
+		let plugins = [
+				autoprefixer(
+					{
+						browsers: ['last 10 versions']
+					}
+				),
+				cssnano()
+		];
+
+		return multipipe(
+				gulp.src(`${buildPath}/css/main.css`),
+				debug(),
+				postcss(plugins),
+				rename('main.min.css'),
+				debug(),
+				gulp.dest(`${buildPath}/css`)
+		).on('error', notify.onError());
+
+});
+
+// build all
+gulp.task("build", ["pug:build", "css:build", "js:build", "img:min"]);
